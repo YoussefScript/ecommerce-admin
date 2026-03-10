ffbbc59d0efd4069fb53600cac4bae71bb05ce91
youssef <youssef1362009kamel@gmail.com>
2026-03-10 18:14:44 +0200

    Categories Entity (Admin)

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
+import { DataTable } from "@/components/ui/data-table";
+import { CategoryColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface CategoryClientProps {
+    data: CategoryColumn[];
+}
+
+export const CategoryClient = ({ data }: CategoryClientProps) => {
+
+    const router = useRouter();
+    const params = useParams();
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading 
+                    title={`Categories (${data.length})`}
+                    description="Manage your categories" 
+                />
+                <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+            <DataTable columns={columns} data={data} searchKey="name"/>
+            <Heading 
+                title="API"
+                description="API calls for categories"
+            />
+            <Separator />
+            <ApiList entityName="categories" entityIdName="categoryId"/>        </>
+    );
+};

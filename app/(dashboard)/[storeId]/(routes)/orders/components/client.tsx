c686fb22ee7529bfba98fb4ae54e94912b48e5b7
youssef <youssef1362009kamel@gmail.com>
2026-03-11 20:28:33 +0200

    Orders Entity (Admin)

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { OrderColumn, columns } from "./columns";
+interface OrderClientProps {
+    data: OrderColumn[];
+}
+
+export const OrderClient = ({ data }: OrderClientProps) => {
+    return (
+        <>
+            <Heading 
+                title={`Orders (${data.length})`}
+                description="Manage your orders" 
+            />
+            <Separator />
+            <DataTable columns={columns} data={data} searchKey="products"/>
+        </>
+    );
+};
